4c68d2e8fb993c042810bb363aad3b6f64dd0365
Velkarii <dawbor589@gmail.com>
2025-05-04 22:22:37 +0200

    UpdatedMovement_3rdPersonCamera

---
+++ Mekreth/Assets/Movement/FinalCharachterController/Scripts/PlayerLocomotionInput.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Movement.FinalCharacterController
+{
+    [DefaultExecutionOrder(-2)]
+    public class PlayerLocomotionInput : MonoBehaviour, PlayerControls.IPlayerLocomotionMapActions
+    {
+        public PlayerControls PlayerControls { get; private set; }
+        public Vector2 MovementInput { get; private set; }
+        public Vector2 LookInput { get; private set; }
+
+        private void OnEnable()
+        {
+            PlayerControls = new PlayerControls();
+            PlayerControls.Enable();
+
+            PlayerControls.PlayerLocomotionMap.Enable();
+            PlayerControls.PlayerLocomotionMap.SetCallbacks(this);
+        }
+
+        private void OnDisable()
+        {
+            PlayerControls.PlayerLocomotionMap.Disable();
+            PlayerControls.PlayerLocomotionMap.RemoveCallbacks(this);
+        }
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            MovementInput = context.ReadValue<Vector2>();
+            print(MovementInput);
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            LookInput = context.ReadValue<Vector2>();
+        }
+    }
+}
+
